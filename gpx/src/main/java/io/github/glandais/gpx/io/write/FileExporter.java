87d67e26c8099d4797506390fb5fec8b60ac41f6
Gabriel Landais <gabriel.landais@gmail.com>
2025-06-06 00:22:53 +0200

    visu

--- gpx/src/main/java/io/github/glandais/gpx/io/write/FileExporter.java
+++ gpx/src/main/java/io/github/glandais/gpx/io/write/FileExporter.java
@@ -3,7 +3,8 @@ package io.github.glandais.gpx.io.write;
 import io.github.glandais.gpx.data.GPXPath;
 import java.io.File;
 import java.io.IOException;
+import java.io.Writer;
 
 public interface FileExporter {
-    void writeGPXPath(GPXPath path, File file) throws IOException;
+    void writeGPXPath(GPXPath path, File writer) throws IOException;
 }
